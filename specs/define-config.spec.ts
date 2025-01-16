42847b4d1704f3f1b49cf52ea7b9171d4ccb0b1a
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-01-17 02:04:30 +0300

    spec: rewrite

--- specs/define-config.spec.ts
+++ specs/define-config.spec.ts
@@ -1,33 +1,57 @@
 import { describe, expect, test as spec } from 'bun:test';
 
 import { defineConfig } from '#src/index.ts';
+import type { UserConfig } from '#types/user-config.ts';
 
-describe('Semantic Release Config', async () => {
-  spec('should return empty config', async () => {
-    expect(defineConfig({}));
+describe('Semantic Release Config', () => {
+  spec('should return empty config', () => {
+    const config = defineConfig({});
+
+    expect(config).toMatchObject({});
+    expect(config satisfies UserConfig).toBeDefined();
   });
 
-  spec('should return config', async () => {
-    expect(
-      defineConfig({
-        branches: [
-          'main',
-          {
-            channel: 'next',
-            name: 'alpha',
-            prerelease: true,
-            range: '',
-          },
-        ],
-        ci: false,
-        debug: false,
-        dryRun: false,
-        extends: [],
-        plugins: [],
-        preset: '',
-        repositoryUrl: '',
-        tagFormat: '',
-      }),
-    );
+  spec('should return config with all properties', () => {
+    const config = defineConfig({
+      branches: [
+        'main',
+        {
+          channel: 'next',
+          name: 'alpha',
+          prerelease: true,
+          range: '',
+        },
+      ],
+      ci: false,
+      debug: false,
+      dryRun: false,
+      extends: [],
+      plugins: [],
+      preset: '',
+      repositoryUrl: '',
+      tagFormat: '',
+    });
+
+    expect(config).toMatchObject({
+      branches: [
+        'main',
+        {
+          channel: 'next',
+          name: 'alpha',
+          prerelease: true,
+          range: '',
+        },
+      ],
+      ci: false,
+      debug: false,
+      dryRun: false,
+      extends: [],
+      plugins: [],
+      preset: '',
+      repositoryUrl: '',
+      tagFormat: '',
+    });
+
+    expect(config satisfies UserConfig).toBeDefined();
   });
 });
