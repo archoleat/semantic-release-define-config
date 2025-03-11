86fea757990a0494fb17b4b790ef24143129f6f8
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-03-11 15:03:05 +0300

    refactor: define-config.spec.ts
    Signed-off-by: nikkeyl <131481562+nikkeyl@users.noreply.github.com>

--- specs/define-config.spec.ts
+++ specs/define-config.spec.ts
@@ -3,35 +3,35 @@ import { describe, expect, test as spec } from 'bun:test';
 import { defineConfig } from '#src/index.ts';
 import type { UserConfig } from '#types/user-config.ts';
 
+const config = defineConfig({
+  branches: [
+    'main',
+    {
+      channel: 'next',
+      name: 'alpha',
+      prerelease: true,
+      range: '',
+    },
+  ],
+  ci: false,
+  debug: false,
+  dryRun: false,
+  extends: [],
+  plugins: [],
+  preset: '',
+  repositoryUrl: '',
+  tagFormat: '',
+});
+
 describe('Semantic Release Config', () => {
   spec('should return empty config', () => {
-    const config = defineConfig({});
+    const emptyConfig = defineConfig({});
 
-    expect(config).toMatchObject({});
-    expect(config satisfies UserConfig).toBeDefined();
+    expect(emptyConfig).toMatchObject({});
+    expect(emptyConfig satisfies UserConfig).toBeDefined();
   });
 
   spec('should return config with all properties', () => {
-    const config = defineConfig({
-      branches: [
-        'main',
-        {
-          channel: 'next',
-          name: 'alpha',
-          prerelease: true,
-          range: '',
-        },
-      ],
-      ci: false,
-      debug: false,
-      dryRun: false,
-      extends: [],
-      plugins: [],
-      preset: '',
-      repositoryUrl: '',
-      tagFormat: '',
-    });
-
     expect(config).toMatchObject({
       branches: [
         'main',
